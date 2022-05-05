c152cac866eb56dc147d52eceab53a5034ab0295
BatyrArtem <artembatyr88@gmail.com>
2022-05-06 02:21:54 +0300

    change photo

--- dist/js/main.js
+++ dist/js/main.js
@@ -4,9 +4,13 @@ const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
 const navItems = document.querySelectorAll('.nav-items');
+const aboutMe = document.getElementById("about-info")
+const portrait = document.getElementById("portrait")
 
 //Set Initial State Of Menu
 let showMenu = false;
+let changeImage = true;
+let changeImageMenu = true;
 
 menuBtn.addEventListener('click', toggleMenu);
 
@@ -31,3 +35,50 @@ function toggleMenu() {
         showMenu = false;
     }
 }
+
+// function changePhoto() {
+//     setTimeout( x => {
+//         aboutMe.classList.remove("bio-image")
+//         aboutMe.classList.add("hide")
+//     }, 1500)
+// }
+
+function changePhotoInterval() {
+        changeImage = !changeImage   
+        aboutMe.classList.remove("bio-image") 
+        aboutMe.classList.add("hide")
+    setInterval(x => {
+        if(changeImage) {
+            changeImage = !changeImage
+            aboutMe.classList.remove("bio-image-back") 
+            aboutMe.classList.add("hide")
+        }
+        else {
+            changeImage = !changeImage
+            aboutMe.classList.remove("hide")
+            aboutMe.classList.add("bio-image-back")
+        }
+    }, 1500)
+}
+
+changePhotoInterval()
+
+// function changePhotoMenu() {
+//         changeImageMenu = !changeImageMenu   
+//         portrait.classList.remove("portrait") 
+//         portrait.classList.add("portrait-hide")
+//     setInterval(x => {
+//         if(changeImageMenu) {
+//             changeImageMenu = !changeImageMenu
+//             portrait.classList.remove("portrait-back") 
+//             portrait.classList.add("portrait-hide ")
+//         }
+//         else {
+//             changeImageMenu = !changeImageMenu
+//             portrait.classList.remove("portrait-hide")
+//             portrait.classList.add("portrait-back")
+//         }
+//     }, 1500)
+// }
+
+// changePhotoMenu()
