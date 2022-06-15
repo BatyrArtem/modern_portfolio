cbda96662ea5d6c825d1a9597b2ccd419564fdb3
BatyrArtem <artembatyr88@gmail.com>
2022-06-15 11:22:14 +0300

    Updates

--- js/main.js
+++ js/main.js
@@ -4,13 +4,8 @@ const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
 const navItems = document.querySelectorAll('.nav-item');
-const aboutMe = document.getElementById("about-info")
-const portrait = document.getElementById("portrait")
 
-//Set Initial State Of Menu
 let showMenu = false;
-let changeImage = true;
-let changeImageMenu = true;
 
 menuBtn.addEventListener('click', toggleMenu);
 
@@ -22,7 +17,6 @@ function toggleMenu() {
         menuBranding.classList.add('show');
         navItems.forEach(item => item.classList.add('show'));
 
-        //Set Menu State
         showMenu = true;
     } else  {
         menuBtn.classList.remove('close');
@@ -31,54 +25,6 @@ function toggleMenu() {
         menuBranding.classList.remove('show');
         navItems.forEach(item => item.classList.remove('show'));
 
-        //Set Menu State
         showMenu = false;
     }
 }
-
-// function changePhoto() {
-//     setTimeout( x => {
-//         aboutMe.classList.remove("bio-image")
-//         aboutMe.classList.add("hide")
-//     }, 1500)
-// }
-
-function changePhotoInterval() {
-        changeImage = !changeImage   
-        aboutMe.classList.remove("bio-image") 
-        aboutMe.classList.add("hide")
-    setInterval(x => {
-        if(changeImage) {
-            changeImage = !changeImage
-            aboutMe.classList.remove("bio-image-back") 
-            aboutMe.classList.add("hide")
-        }
-        else {
-            changeImage = !changeImage
-            aboutMe.classList.remove("hide")
-            aboutMe.classList.add("bio-image-back")
-        }
-    }, 1500)
-}
-
-changePhotoInterval()
-
-// function changePhotoMenu() {
-//         changeImageMenu = !changeImageMenu   
-//         portrait.classList.remove("portrait") 
-//         portrait.classList.add("portrait-hide")
-//     setInterval(x => {
-//         if(changeImageMenu) {
-//             changeImageMenu = !changeImageMenu
-//             portrait.classList.remove("portrait-back") 
-//             portrait.classList.add("portrait-hide ")
-//         }
-//         else {
-//             changeImageMenu = !changeImageMenu
-//             portrait.classList.remove("portrait-hide")
-//             portrait.classList.add("portrait-back")
-//         }
-//     }, 1500)
-// }
-
-// changePhotoMenu()
