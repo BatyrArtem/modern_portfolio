5734489fcd56f7013c16f40772286fb0548268a5
BatyrArtem <artembatyr88@gmail.com>
2022-06-16 18:13:27 +0300

    add min time load 1 sec

--- dist/js/loader.js
+++ dist/js/loader.js
@@ -1,6 +1,10 @@
 let spinnerWrapper = document.querySelector('.spinner-wrapper');
 
+const pageStart = Date.now()
+
 window.addEventListener('load', function () {
+    const timeLoaded = Date.now() - pageStart
     // spinnerWrapper.parentElement.removeChild(spinnerWrapper);
-    spinnerWrapper.classList.add('fadeOut')
+    if(timeLoaded < 1000) setTimeout(() => spinnerWrapper.classList.add('fadeOut'), 1000 - timeLoaded)
+    else spinnerWrapper.classList.add('fadeOut')
 });
