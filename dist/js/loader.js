0e72e595a6880974050e36b78bbdc60a3423dc1e
BatyrArtem <artembatyr88@gmail.com>
2022-06-17 12:55:57 +0300

    clean css

--- dist/js/loader.js
+++ dist/js/loader.js
@@ -3,8 +3,13 @@ let spinnerWrapper = document.querySelector('.spinner-wrapper');
 const pageStart = Date.now()
 
 window.addEventListener('load', function () {
-    const timeLoaded = Date.now() - pageStart
     // spinnerWrapper.parentElement.removeChild(spinnerWrapper);
-    if(timeLoaded < 1000) setTimeout(() => spinnerWrapper.classList.add('fadeOut'), 1000 - timeLoaded)
-    else spinnerWrapper.classList.add('fadeOut')
+    const timeLoaded = Date.now() - pageStart
+
+    function classAddSpinner() {
+        spinnerWrapper.classList.add('fadeOut')
+    }
+
+    if(timeLoaded < 1000) setTimeout(() => classAddSpinner(), 750 - timeLoaded)
+    else classAddSpinner()
 });
