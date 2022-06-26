8242e71a136e60b8fd26749f8b456625be1ca001
BatyrArtem <artembatyr88@gmail.com>
2022-06-26 14:37:39 +0300

    add responsive forest

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,9 +1,14 @@
-// Select DOM Items
 const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
 const navItems = document.querySelectorAll('.nav-item');
+// const get = element => document.querySelector(element)
+
+// const menuBtn = get('.menu-btn');
+// const menu = get('.menu');
+// const menuNav = get('.menu-nav');
+// const menuBranding = get('.menu-branding');
 
 let showMenu = false;
 
